e2bdf772c602622ea8c159f2b902d41a619e7927
unknown <mechanical.maggot.0@gmail.com>
2016-06-01 12:35:31 +0100

    e assim que se converte pa json? ninguem sabe

--- TBDA3.sql
+++ TBDA3.sql
@@ -29,40 +29,42 @@ AS
   v_queryctx       DBMS_XMLQuery.ctxType;
   v_clob_par       CLOB;
   BEGIN
-  v_sqlselect := 'SELECT l.codigo, l.sigla, l.nome
-  FROM LICS l
-  ';
-  v_queryctx := DBMS_XMLQuery.newContext(v_sqlselect);
-  DBMS_XMLQuery.setEncodingTag(v_queryctx, 'ISO-8859-1');
-  v_clob_par := DBMS_XMLQuery.getXML(v_queryctx);
-  DBMS_XMLQuery.closeContext(v_queryctx);
-  htp.p(v_clob_par);
-  htp.para;
-  EXCEPTION
-  WHEN OTHERS THEN
-    htp.p(SQLERRM);
+    v_sqlselect := 'SELECT l.codigo, l.sigla, l.nome
+    FROM LICS l
+    ';
+    v_queryctx := DBMS_XMLQuery.newContext(v_sqlselect);
+    DBMS_XMLQuery.setEncodingTag(v_queryctx, 'ISO-8859-1');
+    v_clob_par := DBMS_XMLQuery.getXML(v_queryctx);
+    DBMS_XMLQuery.closeContext(v_queryctx);
+    htp.p('<?xml-stylesheet type="text/xsl" href="xml2json.xsl"?>');
+    htp.p(v_clob_par);
+    htp.para;
+    EXCEPTION
+    WHEN OTHERS THEN
+      htp.p(SQLERRM);
   END list_programs; 
 
   PROCEDURE list_applications
   IS
-  v_sqlselect      VARCHAR2(30000);
+  v_sqlselect      VARCHAR2(2000);
   v_queryctx       DBMS_XMLQuery.ctxType;
   v_clob_par        CLOB;
   v_offset number default 1;
   v_chunk_size number := 10000;
- begin
-  v_sqlselect := 'SELECT c.BI, c.CURSO, c.ANO_LECTIVO, c.RESULTADO, c.MEDIA
-  FROM CANDS c
-  ';
-  v_queryctx := DBMS_XMLQuery.newContext(v_sqlselect);
-  DBMS_XMLQuery.setEncodingTag(v_queryctx, 'ISO-8859-1');
-  v_clob_par := DBMS_XMLQuery.getXML(v_queryctx);
-  DBMS_XMLQuery.closeContext(v_queryctx);
-  loop
-          exit when v_offset > dbms_lob.getlength(v_clob_par);
-          htp.p( dbms_lob.substr( v_clob_par, v_chunk_size, v_offset ) );
-          v_offset := v_offset +  v_chunk_size;
-      end loop;
+  begin
+    v_sqlselect := 'SELECT c.BI, c.CURSO, c.ANO_LECTIVO, c.RESULTADO, c.MEDIA
+    FROM CANDS c
+    ';
+    v_queryctx := DBMS_XMLQuery.newContext(v_sqlselect);
+    DBMS_XMLQuery.setEncodingTag(v_queryctx, 'ISO-8859-1');
+    v_clob_par := DBMS_XMLQuery.getXML(v_queryctx);
+    DBMS_XMLQuery.closeContext(v_queryctx);
+    loop
+      exit when v_offset > dbms_lob.getlength(v_clob_par);
+      htp.p( dbms_lob.substr( v_clob_par, v_chunk_size, v_offset ) );
+      htp.para;
+      v_offset := v_offset +  v_chunk_size;
+    end loop;
   END list_applications; 
 
   PROCEDURE list_enrolments
@@ -70,16 +72,24 @@ AS
   v_sqlselect      VARCHAR2(2000);
   v_queryctx       DBMS_XMLQuery.ctxType;
   v_clob_par       CLOB;
+  v_offset number default 1;
+  v_chunk_size number := 10000;
   BEGIN
-  v_sqlselect := 'SELECT *
-      FROM ALUS  
-      ';
-  v_queryctx := DBMS_XMLQuery.newContext(v_sqlselect);
-  DBMS_XMLQuery.setEncodingTag(v_queryctx, 'ISO-8859-1');
-  v_clob_par := DBMS_XMLQuery.getXML(v_queryctx);
-  DBMS_XMLQuery.closeContext(v_queryctx);
-  htp.p(v_clob_par);
-  htp.para;
+    v_sqlselect := 'SELECT a.NUMERO, a.BI, a.CURSO, a.A_LECT_MATRICULA, a.ESTADO, a.A_LECT_CONCLUSAO, a.MED_FINAL
+        FROM ALUS a 
+        ';
+    v_queryctx := DBMS_XMLQuery.newContext(v_sqlselect);
+    DBMS_XMLQuery.setEncodingTag(v_queryctx, 'ISO-8859-1');
+    DBMS_XMLQUERY.SETSTYLESHEETHEADER(v_queryctx, 'xml2json.xsl');
+    v_clob_par := DBMS_XMLQuery.getXML(v_queryctx);
+    
+    DBMS_XMLQuery.closeContext(v_queryctx);
+    loop
+      exit when v_offset > dbms_lob.getlength(v_clob_par);
+      htp.p( dbms_lob.substr( v_clob_par, v_chunk_size, v_offset ) );
+      htp.para;
+      v_offset := v_offset +  v_chunk_size;
+    end loop;
   EXCEPTION
   WHEN OTHERS THEN
     htp.p(SQLERRM);
@@ -93,8 +103,8 @@ AS
   v_queryctx       DBMS_XMLQuery.ctxType;
   v_clob_par       CLOB;
   BEGIN
-  v_sqlselect := 'SELECT *
-      FROM anos     
+  v_sqlselect := 'SELECT an.ANO
+      FROM anos an    
       ';
   v_queryctx := DBMS_XMLQuery.newContext(v_sqlselect);
   DBMS_XMLQuery.setEncodingTag(v_queryctx, 'ISO-8859-1');
